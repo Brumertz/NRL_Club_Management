61d2b9502e68d80e44d5b714070a1d3332b28deb
Brandon Morales Cordoba <160804600+Brumertz@users.noreply.github.com>
2024-11-01 11:19:19 +0800

    Final Project Relational DataBase
    start final project

---
+++ Database_NRLClub/Database_NRLClub/dbo.Ladder.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Ladder]
+(
+    [LadderID] INT IDENTITY(1, 1) PRIMARY KEY,  -- ID auto-incremental
+    [ClubID] INT NOT NULL,
+    [Position] INT NOT NULL,
+    [Points] INT NOT NULL,
+    [GamesWon] INT NOT NULL,
+    [GamesLost] INT NOT NULL,
+    FOREIGN KEY ([ClubID]) REFERENCES [dbo].[Club]([ClubID]) ON DELETE CASCADE
+);
---
+++ NRL_Club_Management_WebSide/NRL_Club_Management_WebSide/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NRL_Club_Management_WebSide.MvcApplication" Language="C#" %>
